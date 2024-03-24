133cd461197d670884637d614d95df3de6c31287
jigarlpatel <jigarlpatel@gmail.com>
2024-03-24 13:34:35 -0500

    Try to resolve error with git 40 char hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '553f323ffe8c3b45e873cb8e05e8674f005e7cf3'
+AESD_ASSIGNMENTS_VERSION = '3dbb839'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
