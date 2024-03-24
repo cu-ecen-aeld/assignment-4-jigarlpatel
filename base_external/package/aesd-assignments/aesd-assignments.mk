5ebdf72222ccfe9c60fcadae38906eb5eb27052c
jigarlpatel <jigarlpatel@gmail.com>
2024-03-24 13:09:25 -0500

    added correct link.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '67b63970cbbd2a567944841e6c04684faf053951'
+AESD_ASSIGNMENTS_VERSION = '553f323ffe8c3b45e873cb8e05e8674f005e7cf3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-nguyenhungcuong.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jigarlpatel.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
